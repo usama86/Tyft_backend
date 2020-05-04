c6898bc268f6a1b1d7e5f2c46156f243ebd62897
Usama <usama.amjad32@gmail.com>
2020-05-05 01:38:56 +0500

    Image upload done

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,42 @@
 import userService from '../services/userService'; 
 import CheckAuth from './../middlewae/checkAuth';
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+	destination: function(req,file,cb)
+	{
+		cb(null, __dirname + './../uploads/')
+	},
+	filename: function (req, file, cb) {
+        cb(null , file.originalname);
+    }
+})
+const fileFilter = (req, file, cb) =>{
+	if(file.mimetype === 'images/jpeg' || file.mimetype === 'images/png')
+		cb(null, true);
+	else{
+		cb(null,false);
+	}
+
+};
+
+const upload = multer({
+	storage:storage,
+	limits:{
+		fileSize: 1024 * 1024 * 10  //max 10 mb
+	},
+	fileFilter:fileFilter
+});
+
+
 module.exports = (router) => {
   /*
 	router.post('/saveForumPost', forumService.addNewForum);	
 	router.get('/getPopularPost', forumService.getPopular);
 */
 	  router.get('/getUser', userService.getAllUser);
-	  router.post('/signup', userService.signup);
-	  router.post('/login', userService.login);
+	  router.post('/signup',upload.array('Image'), userService.signup);
+	  router.post('/log in', userService.login);
 	return router;
 };
 //CheckAuth
