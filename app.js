5da7fd8657dd4f989bd4dfd1b1c2c4d5158bcfb8
Usama <usama.amjad32@gmail.com>
2020-05-12 01:12:40 +0500

    'hi'

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const rateLimit = require('express-rate-limit');
 // Database connection setup
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true });
-const db = mongoose.connection;
+const db = mongoose.connection; 
 db.on('error', function(err) {
 	console.error('connection error:', err);
 	process.exit(1); 
