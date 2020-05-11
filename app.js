ed8c3727a6775584c5e13903758606e3f9e88521
Usama <usama.amjad32@gmail.com>
2020-05-11 23:45:43 +0500

    make it better

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const rateLimit = require('express-rate-limit');
 // Database connection setup
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true });
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true });
 const db = mongoose.connection;
 db.on('error', function(err) {
 	console.error('connection error:', err);
--- services/userService.js
+++ services/userService.js
@@ -120,6 +120,7 @@ export default {
                   userId: user[0]._id,
                   userType: user[0].userType,
                   profileName: user[0].profileName,
+                  
                 },
                 "Bearer" //process.env.xyz
                 // {
