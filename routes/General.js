62710a53f159a4e044af7fc7d58d91c1d5c28c6d
Usama <usama.amjad32@gmail.com>
2021-01-04 01:20:35 +0500

    addradius

--- routes/General.js
+++ routes/General.js
@@ -3,7 +3,8 @@ const GeneralService = require('../services/GeneralService');
 
 module.exports = (router) => {
 
-	  router.get('/getradius', GeneralService.getRadius);
+      router.get('/getradius', GeneralService.getRadius);
+      router.post('/addradius', GeneralService.AddRadius);
 	  router.post('/updateradius', GeneralService.UpdateRadius);
 
 	return router;
--- services/GeneralService.js
+++ services/GeneralService.js
@@ -10,6 +10,19 @@ module.exports = {
 			res.json({ code: 'ABT0001' });
 		}
 	},
+	async AddRadius(req, res) {
+		try {
+			let Radius = {
+				MapRadius: 16093.4
+			};
+			const Radiuss = new General(Radius);
+            await Radiuss.save();
+            res.json({ code: 'ABT000' });
+		} catch (e) {
+			console.log('error saving Map Radius', e);
+			res.json({ code: 'ABT0001' });
+		}
+	},
 	async UpdateRadius(req, res) {
 		try {
 			let reqBody = req.body;
