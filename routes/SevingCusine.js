24336f7f4e7d94cc0c82973b148e9a8d4631bc70
Usama <usama.amjad32@gmail.com>
2021-04-03 02:45:43 +0500

    added cusine api

--- routes/SevingCusine.js
+++ routes/SevingCusine.js
@@ -5,6 +5,7 @@ module.exports = (router) => {
 
 	  router.get('/getcusines', CusineService.getCusines);
 	  router.post('/addcusines', CusineService.addCusines);
+	  router.post('/updatecusines', CusineService.updateCusine);
 	  router.post('/deletecusines', CusineService.deleteCusines);
 
 	return router;
